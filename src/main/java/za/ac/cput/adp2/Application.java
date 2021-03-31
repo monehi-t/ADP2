0d2c2e6b30fb60048e35cdfb65fd765e56366f88
admin <admin@DESKTOP-CAMERON>
2021-03-31 21:29:17 +0200

    Added: Classes TDD -Object Identity -Object Equality -Failing Test -Disabling Test -TimeOuts

--- src/main/java/za/ac/cput/adp2/Application.java
+++ src/main/java/za/ac/cput/adp2/Application.java
@@ -7,11 +7,29 @@ package za.ac.cput.adp2;
 
 /**
  *
- * @author CPUT
+ * @author Cameron Van Wyk 219076936
  */
 public class Application {
     
     public static void main(String[] args) {
-        System.out.println("Hi!! =)");
+        
+    }
+    private String id, name, appType;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAppType() {
+        return appType;
+    }
+     @Override
+    public String toString() {
+        return "Application{" + "id=" + id + ", name=" + name + ", appType=" + appType + '}';
     }
 }
--- src/test/java/za/ac/cput/adp2/ApplicationTest.java
+++ src/test/java/za/ac/cput/adp2/ApplicationTest.java
@@ -5,48 +5,68 @@
  */
 package za.ac.cput.adp2;
 
+import java.util.concurrent.TimeUnit;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Timeout;
 
 /**
  *
- * @author CPUT
+ * @author Cameron Van Wyk 219076936
  */
 public class ApplicationTest {
+    
+    private Application app1;
+    private Application app2;
+    private Application app3;
+    
     
     public ApplicationTest() {
     }
     
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
     @BeforeEach
     public void setUp() {
+        app1 = new Application();
+        app2 = new Application();
+        app1 = app3;  
     }
-    
-    @AfterEach
-    public void tearDown() {
+    //Object Identity
+    @Test
+    void testIdentity() {
+        assertSame(app1,app3);
     }
 
-    /**
-     * Test of main method, of class Application.
-     */
+    //Object Equality
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        Application.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    void testEquality() {
+        assertEquals(app1,app3);
+    }
+    
+    //Failing Test
+    @Test
+    void testAppTest() {
+        assertEquals(app1,app2);
+        fail();
+    }
+    
+    //Disabling Test
+    @Test
+    @Disabled
+    void testName() {
+        assertSame(app3,app2);
+    }
+    
+    //Timeouts
+    // timed out after 500 miliseconds
+    @Test
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    void test_this() {
     }
     
 }
